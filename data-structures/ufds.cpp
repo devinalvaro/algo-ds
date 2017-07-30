9583d9f53e0272876cdd927f0da4fa333ca9ce09
Devin Alvaro <devin.alvaro@gmail.com>
2017-07-30 20:10:56 +0700

    Add ufds.cpp

---
+++ data-structures/ufds.cpp
@@ -0,0 +1,42 @@
+/* Union-Find Disjoint Sets with rank (level) */
+
+#include <vector>
+
+using namespace std;
+
+class UFDS {
+private:
+    vector<int> parent;
+    vector<int> level;
+
+public:
+    int find(int u) { return (parent[u] == u ? u : find(parent[u])); }
+
+    void merge(int u, int v) {
+        int ancestor_u = find(u);
+        int ancestor_v = find(v);
+
+        if (ancestor_u == ancestor_v) {
+            return;
+        }
+
+        if (level[ancestor_u] > level[ancestor_v]) {
+            parent[ancestor_v] = ancestor_u;
+        } else {
+            parent[ancestor_u] = ancestor_v;
+
+            if (level[ancestor_u] == level[ancestor_v]) {
+                level[ancestor_v]++;
+            }
+        }
+    }
+
+    UFDS(int n) {
+        parent.assign(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        level.assign(n + 1, 1);
+    }
+};
