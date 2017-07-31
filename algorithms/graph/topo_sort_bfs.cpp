990bd366303688e97bfc97ee4236e6c396fa208d
Devin Alvaro <devin.alvaro@gmail.com>
2017-07-31 12:40:03 +0700

    Add topo_sort_bfs.cpp

---
+++ algorithms/graph/topo_sort_bfs.cpp
@@ -0,0 +1,39 @@
+/* Topological Sort with breadth-first search for directed acyclic graph */
+
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+/**
+ * n: number of elements to topologically sorted
+ * edges: edges[u][j] = v means u is adjacent or neighbor with v
+ * in_degrees: in_degrees[u] means number of nodes adjacent with u
+ * topo_array: resulting array of topologically sorted elements
+ */
+void topo_sort_bfs(int n, const vector<vector<int>> &edges,
+                   vector<int> &in_degrees, vector<int> &topo_array) {
+    // priority queue with minimum element on top
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 1; i <= n; i++) {
+        if (in_degrees[i] == 0) {
+            pq.push(i);
+        }
+    }
+
+    while (!pq.empty()) {
+        int u = pq.top();
+        pq.pop();
+
+        topo_array.push_back(u);
+
+        for (size_t j = 0; j < edges[u].size(); j++) {
+            int v = edges[u][j];
+
+            in_degrees[v]--;
+            if (in_degrees[v] == 0) {
+                pq.push(v);
+            }
+        }
+    }
+}
