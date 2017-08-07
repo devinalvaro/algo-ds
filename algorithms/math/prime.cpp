f59e3022c026628c1c68a31ea454a01c87027b74
Devin Alvaro <devin.alvaro@gmail.com>
2017-08-07 19:04:39 +0700

    Fix bugs in prime.cpp

--- algorithms/math/prime.cpp
+++ algorithms/math/prime.cpp
@@ -1,23 +1,23 @@
 /* Listing prime numbers and primality test with Sieve of Eratosthenes */
 
+#include <bitset>
 #include <vector>
 
 using namespace std;
 
 /*
+ * n: intended size of sieve [1...n]
  * is_prime: is_prime[i] means number i is prime
- * primes: resulting array of prime numbers from [1...is_prime.size()]
  */
-void sieve_of_eratosthenes(vector<bool> &is_prime, vector<int> &primes)
+void sieve_of_eratosthenes(int n, bitset<10000010> &is_prime)
 {
-    is_prime[0] = is_prime[1] = false;
+    is_prime.set();
+    is_prime[0] = is_prime[1] = 0;
 
-    for (int i = 2; i * i <= is_prime.size(); i++) {
+    for (int i = 2; i * i <= n; i++) {
         if (is_prime[i]) {
-            primes.push_back(i);
-
-            for (int j = 2 * i; j < is_prime.size(); j += i) {
-                is_prime[i] = false;
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = 0;
             }
         }
     }
@@ -25,17 +25,33 @@ void sieve_of_eratosthenes(vector<bool> &is_prime, vector<int> &primes)
 
 /**
  * is_prime: is_prime[i] means number i is prime
- * primes: resulting array of prime numbers from [1...is_prime.size()]
+ * primes: resulting array of prime numbers from [1...n]
+ */
+void find_primes(bitset<10000010> &is_prime, vector<int> &primes)
+{
+    primes.clear();
+
+    for (size_t i = 0; i < is_prime.size(); i++) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+        }
+    }
+}
+
+/**
+ * n: size of sieve [1...n]
+ * is_prime: is_prime[i] means number i is prime
+ * primes: resulting array of prime numbers from [1...n]
  * number: the number to be primality tested
  */
-bool is_prime(vector<bool> &is_prime, vector<int> &primes, int number) {
-    if (number <= primes.size())
+bool is_prime(int n, bitset<10000010> &is_prime, vector<int> &primes, int number)
+{
+    if (number <= n)
         return is_prime[number];
 
-    for (int i = 0; i < primes.size(); i++) {
-        if (number % primes[i] == 0) {
+    for (size_t i = 0; i < primes.size(); i++) {
+        if (number % primes[i] == 0)
             return false;
-        }
     }
 
     return true;
@@ -46,10 +62,12 @@ bool is_prime(vector<bool> &is_prime, vector<int> &primes, int number) {
  * number: the number to be factored by prime numbers
  * prime_factors: resulting array of the number's prime factors
  */
-void prime_factors(vector<int> &primes, int number, vector<int> &prime_factors) {
+void prime_factors(vector<int> &primes, int number, vector<int> &prime_factors)
+{
     prime_factors.clear();
 
-    for (int i = 0; i < primes.size() && primes[i] * primes[i] <= number; i++) {
+    for (size_t i = 0; i < primes.size() && primes[i] * primes[i] <= number;
+         i++) {
         while (number % primes[i] == 0) {
             number /= primes[i];
 
